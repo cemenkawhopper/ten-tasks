0e9de8f2b9a3ab028b68e20d55267a0539aeee95
cemenkawhopper <deadinside0088@gmail.com>
2024-09-27 09:30:33 +0700

    Add files via upload

---
+++ semyon.py
@@ -0,0 +1,9 @@
+try:
+    number = int(input(" "))
+
+    if number % 2 == 0:
+        print("Число четное")
+    else:
+        print("Число нечетное")
+except ValueError:
+    print("введи нормальное число гав гав гав")
