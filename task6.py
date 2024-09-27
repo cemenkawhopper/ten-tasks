0b73565f796649351ec257a530f0e627fd6c43c9
cemenkawhopper <deadinside0088@gmail.com>
2024-09-27 09:52:30 +0700

    Add files via upload

---
+++ task6.py
@@ -0,0 +1,8 @@
+num1 = int(input(" "))
+num2 = int(input(" "))
+num3 = int(input(" "))
+
+if num1 % 2 == 0 or num2 % 2 == 0 or num3 % 2 == 0:
+    print("Есть четное число")
+else:
+    print("Нет четных чисел")
