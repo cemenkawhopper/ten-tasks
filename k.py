bcc873cdce3b4c65824e56c684d927a3b0ba2103
cemenkawhopper <deadinside0088@gmail.com>
2024-09-27 09:26:24 +0700

    Add files via upload

---
+++ k.py
@@ -0,0 +1,9 @@
+try:
+    number = float(input())
+
+    if number > 0:
+        print("Число положительное")
+    else:
+        print("Число не положительное")
+except ValueError:
+    print("инвалид чтоли? введи число")
