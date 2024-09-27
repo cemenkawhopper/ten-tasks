cbf6f4d561844c9e95e1c2cf9a474c7e8244d649
cemenkawhopper <deadinside0088@gmail.com>
2024-09-27 09:38:08 +0700

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,9 @@
+try:
+    number = int(input(" "))
+
+    if 10 <= number <= 20:
+        print("Число попадает в диапазон ")
+    else:
+        print("Число не попадает в диапазон")
+except ValueError:
+    print("введи нормальное число")
